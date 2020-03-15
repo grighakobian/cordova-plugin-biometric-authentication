0f47e378e10964361e2b91fd30844ae36f780083
Grigor Hakobyan <grighakobian@gmail.com>
2020-03-16 03:39:54 +0400

    Added firebase configuration

--- src/ios/src/BiometricAuthentication.swift
+++ src/ios/src/BiometricAuthentication.swift
@@ -1,10 +1,13 @@
 import UIKit
+import FirebaseCore
 
 @objc(BiometricAuthentication)
 class BiometricAuthentication : CDVPlugin {
     
     @objc(coolMethod:)
     func coolMethod(_ command: CDVInvokedUrlCommand) {
+         FirebaseApp.configure()
+         
         let window = UIApplication.shared.keyWindow
         let rootViewController = window?.rootViewController
         let livenessViewController = OZLivenessViewController()
