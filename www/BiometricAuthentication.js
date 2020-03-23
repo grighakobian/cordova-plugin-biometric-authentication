4732dea1f0599198b58d1dbaa53f1ac1990317ae
Grigor Hakobyan <grighakobian@gmail.com>
2020-03-23 18:20:38 +0400

    Fix

--- www/BiometricAuthentication.js
+++ www/BiometricAuthentication.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
 exports.analyze = function (arg0, success, error) {
-    exec(success, error, 'BiometricAuthentication', 'coolMethod', [arg0]);
+    exec(success, error, 'BiometricAuthentication', 'analyze', [arg0]);
 };
