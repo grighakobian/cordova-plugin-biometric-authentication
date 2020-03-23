519d575e069fd82c63790831c4f8561001edee1c
Grigor Hakobyan <grighakobian@gmail.com>
2020-03-23 13:41:30 +0400

    Fixed compile issue

--- src/ios/src/UIColor+Hex.swift
+++ src/ios/src/UIColor+Hex.swift
@@ -8,12 +8,11 @@
 import UIKit
 
 public extension UIColor {
-    @nonobjc static func hexa(_ value: UInt32) -> UIColor {
-        let a = CGFloat((value & 0xFF000000) >> 24) / 255.0
+    @nonobjc static func hex(_ value: UInt32) -> UIColor {
         let r = CGFloat((value & 0xFF0000) >> 16) / 255.0
         let g = CGFloat((value & 0xFF00) >> 8) / 255.0
         let b = CGFloat((value & 0xFF)) / 255.0
         
-        return UIColor(red: r, green: g, blue: b, alpha: a)
+        return UIColor(red: r, green: g, blue: b, alpha: 1.0)
     }
 }
