bcffa81425cebb9f347cd03d9920157f0fc31b33
Grigor Hakobyan <grighakobian@gmail.com>
2020-04-05 22:38:39 +0400

    Bump to version 1.1.5

--- src/ios/sdk/OZFrameViewController.swift
+++ src/ios/sdk/OZFrameViewController.swift
@@ -229,9 +229,9 @@ class OZFrameViewController: UIViewController {
     
     lazy var faceDetectorOptions: VisionFaceDetectorOptions = {
         let options = VisionFaceDetectorOptions()
-        options.modeType = .fast
-        options.landmarkType = .all
-        options.classificationType = .all
+        options.performanceMode = .fast
+        options.landmarkMode = .all
+        options.classificationMode = .all
         return options
     }()
     
@@ -255,7 +255,7 @@ class OZFrameViewController: UIViewController {
         let visionImage = VisionImage(buffer: buffer)
         visionImage.metadata = self.metadata
         let faceDetector = vision.faceDetector(options: faceDetectorOptions)
-        faceDetector.detect(in: visionImage) { [weak self] faces, error in
+        faceDetector.process(visionImage) { [weak self] faces, error in
             self?.process(faces: faces ?? []) { [weak self] in
                 self?.blockDetection = false
                 self?.mlKitFPS = 1/CGFloat(-date.timeIntervalSinceNow)
